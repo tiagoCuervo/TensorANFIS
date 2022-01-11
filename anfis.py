1719418938df49082f8a4ad5158fe96086790c15
Mahdi Arjomandazar <86355677+MahdiArj@users.noreply.github.com>
2022-01-11 16:19:33 +0330

    modified-ANFIS
    This modification introduces new feature for predicting via the trained models of TensorANFIS. For this purpose, the functions predict() and hyper_init() were defined to load the saved Tensorflow graphs and sessions for prediction and; initialize independent hyper-parameters, respectively.

--- anfis.py
+++ anfis.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+#import tensorflow.compat.v1 as tf
+#tf.disable_v2_behavior()
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,45 +8,56 @@ import matplotlib.pyplot as plt
 class ANFIS:
 
     def __init__(self, n_inputs, n_rules, learning_rate=1e-2):
+    
         self.n = n_inputs
         self.m = n_rules
         self.inputs = tf.placeholder(tf.float32, shape=(None, n_inputs))  # Input
         self.targets = tf.placeholder(tf.float32, shape=None)  # Desired output
-        mu = tf.get_variable("mu", [n_rules * n_inputs],
-                             initializer=tf.random_normal_initializer(0, 1))  # Means of Gaussian MFS
-        sigma = tf.get_variable("sigma", [n_rules * n_inputs],
-                                initializer=tf.random_normal_initializer(0, 1))  # Standard deviations of Gaussian MFS
-        y = tf.get_variable("y", [1, n_rules], initializer=tf.random_normal_initializer(0, 1))  # Sequent centers
-
+        mu, sigma, y = hyper_init(self.n, self.m)
         self.params = tf.trainable_variables()
-
         self.rul = tf.reduce_prod(
             tf.reshape(tf.exp(-0.5 * tf.square(tf.subtract(tf.tile(self.inputs, (1, n_rules)), mu)) / tf.square(sigma)),
                        (-1, n_rules, n_inputs)), axis=2)  # Rule activations
+        
         # Fuzzy base expansion function:
         num = tf.reduce_sum(tf.multiply(self.rul, y), axis=1)
         den = tf.clip_by_value(tf.reduce_sum(self.rul, axis=1), 1e-12, 1e12)
         self.out = tf.divide(num, den)
-
+    
         self.loss = tf.losses.huber_loss(self.targets, self.out)  # Loss function computation
         # Other loss functions for regression, uncomment to try them:
         # loss = tf.sqrt(tf.losses.mean_squared_error(target, out))
         # loss = tf.losses.absolute_difference(target, out)
         self.optimize = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss)  # Optimization step
         # Other optimizers, uncomment to try them:
-        # self.optimize = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(self.loss)
-        # self.optimize = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(self.loss)
+        #self.optimize = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(self.loss)
+        #self.optimize = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(self.loss)
         self.init_variables = tf.global_variables_initializer()  # Variable initializer
 
-    def infer(self, sess, x, targets=None):
+
+    def infer(self, sess, epoch, num_epochs, x, targets=None):
         if targets is None:
             return sess.run(self.out, feed_dict={self.inputs: x})
         else:
-            return sess.run([self.out, self.loss], feed_dict={self.inputs: x, self.targets: targets})
+            if epoch == num_epochs-1:
+                saver=tf.train.Saver()
+                save_path = saver.save(sess, 'my-model')
+                print("Model saved in path: %s" % save_path)
+                return sess.run([self.out, self.loss], feed_dict={self.inputs: x, self.targets: targets}), saver.save(sess, 'my-model')
+            else:
+                return sess.run([self.out, self.loss], feed_dict={self.inputs: x, self.targets: targets})
 
     def train(self, sess, x, targets):
         yp, l, _ = sess.run([self.out, self.loss, self.optimize], feed_dict={self.inputs: x, self.targets: targets})
         return l, yp
+
+    def predict(self, x):
+        sess=tf.Session()
+        loaded_config = tf.train.import_meta_graph('my-model.meta')
+        loaded_config.restore(sess, tf.train.latest_checkpoint('./'))
+        mu, sigma, y = hyper_init(self.n, self.m)
+        tf.initialize_all_variables().run(session=sess)
+        return sess.run(self.out, feed_dict={self.inputs: x})    # operation = self.out
 
     def plotmfs(self, sess):
         mus = sess.run(self.params[0])
@@ -55,9 +68,21 @@ class ANFIS:
         xn = np.linspace(-1.5, 1.5, 1000)
         for r in range(self.m):
             if r % 4 == 0:
-                plt.figure(figsize=(11, 6), dpi=80)
+                plt.figure(figsize=(11, 6), dpi=300)
             plt.subplot(2, 2, (r % 4) + 1)
             ax = plt.subplot(2, 2, (r % 4) + 1)
             ax.set_title("Rule %d, sequent center: %f" % ((r + 1), y[0, r]))
             for i in range(self.n):
                 plt.plot(xn, np.exp(-0.5 * ((xn - mus[r, i]) ** 2) / (sigmas[r, i] ** 2)))
+                
+def hyper_init(n_inputs, n_rules): # dedicated to initialize mu, sigma and, y values regardless of anfis class
+    with tf.compat.v1.variable_scope('', reuse=tf.AUTO_REUSE):
+            mu = tf.get_variable("mu", [n_rules * n_inputs],
+                           initializer=tf.random_normal_initializer(0, 1))  # Means of Gaussian MFS
+        
+            sigma = tf.get_variable("sigma", [n_rules * n_inputs],
+                                initializer=tf.random_normal_initializer(0, 1))  # Standard deviations of Gaussian MFS
+        
+            y = tf.get_variable("y", [1, n_rules], initializer=tf.random_normal_initializer(0, 1))  # Sequent centers        
+    return mu, sigma, y
+                
